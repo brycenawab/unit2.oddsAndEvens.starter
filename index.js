908444e99b86c17d43ab53f935854956a379c6c6
bryce <brycejnawab@gmail.com>
2024-05-05 14:04:44 -0500

    hi

---
+++ index.js
@@ -0,0 +1,30 @@
+// TODO: this file! :)
+
+const inputBar = document.querySelector("input")
+
+var input = document.querySelector("input").value
+console.log(input)
+
+function bank (event){
+event.preventDefault()
+
+
+const form = document.querySelector("form")
+form.addEventListener("submit" , (event) => {
+    event.preventDefault()
+
+    const p = document.createElement("p")
+    p.innerText = inputBar.value
+    .appendChild(p)
+})
+
+}
+
+function start (){
+    const starting = state.start.map(function(start){
+const p = document.createElement("p")
+const output = document.querySelector("output")
+p.textContent = number
+p.append(output)
+    }
+)}
